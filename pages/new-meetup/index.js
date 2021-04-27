84803687d0f0d7fe6b358fbd190bcff78c6d7e9d
vrnranjan22 <vrnranjan22@gmail.com>
2021-04-27 09:43:33 +0530

    fetch replace by axios

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,19 +1,13 @@
 import NewMeetupFrom from "../../components/meetups/NewMeetupForm";
 import Layout from "../../components/layout/Layout";
+import axios from "axios";
 import { useRouter } from "next/router";
 
 const NewMeetup = () => {
   const router = useRouter();
   const addMeetup = async (val) => {
-    const response = await fetch("/api/new-meetup", {
-      method: "POST",
-      body: JSON.stringify(val),
-      headers: {
-        "Content-Type": "Application/json",
-      },
-    });
-    const data = await response.json();
-    console.log(data);
+    const response = await axios.post("/api/new-meetup", val);
+    console.log(response.data);
     router.push("/");
   };
 
